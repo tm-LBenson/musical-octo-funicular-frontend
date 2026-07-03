c530c29486145fdba5c497528cbca823a288ffd0
Lewis Benson <tm.lewisbenson@gmail.com>
2026-07-02 20:21:39 -0400

    health check working

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
-
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { BACKEND } from "./config";
 
 function App() {
+  const [health, setHealth] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+  async function getHealth() {
+    try {
+      const res = await fetch(BACKEND);
+      const { message } = await res.json();
 
+      setHealth(message);
+    } catch (error) {
+      setError(["Something went wrong with the health check.", error]);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getHealth();
+  }, []);
+
+  if (error) {
+    {
+      console.log(error[0]);
+    }
+    {
+      console.error(error[1]);
+    }
+    return <h2>Something went wrong.</h2>;
+  }
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
   return (
     <>
       <h1>Test</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
